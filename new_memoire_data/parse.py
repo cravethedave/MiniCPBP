f56ef8f89f8410772925ba6388a980e43fddff60
cravethedave <d.saikali@live.com>
2025-07-30 18:11:00 -0400

    Grammar BP removed for testing the algorithm

--- new_memoire_data/parse.py
+++ new_memoire_data/parse.py
@@ -1,85 +1,72 @@
 
 
 
-# rnd_based = set(['maxMarginalStrengthBiasedWheelSelectVal','domWdegRandom'])
-rnd_based = set(['domWdegRandom'])
-# heuristic_based = set(['maxMarginalStrengthLDS', 'maxMarginalStrength', 'maxMarginal', 'maxMarginalLDS'])
-heuristic_based = set([])
-
-instances = [
-    (1750, 2250, -400, -300),
-    (1750, 2250, -200, -100),
-    (1750, 2250, 100, 200),
-
-    (2750, 3250, -400, -300),
-    (2750, 3250, -200, -100),
-    (2750, 3250, 100, 200),
+rnd_based = set(['domWdegRandom', 'maxMarginalStrengthBiasedWheelSelectVal'])
+heuristic_based = set(['maxMarginalStrengthLDS', 'maxMarginalStrength', 'maxMarginal', 'maxMarginalLDS'])
     
-    (3750, 4250, -400, -300),
-    (3750, 4250, -200, -100),
-    (3750, 4250, 100, 200),
-]
-
 def get_median(data: list):
     middle = len(data) // 2
     if len(data) % 2 == 1:
         return data[middle]
     return (data[middle] + data[middle - 1]) / 2
 
-def parse_to_terminal(filename):
+def generic_parse_to_terminal(filename):
     with open(f"./new_memoire_data/{filename}", 'r') as f:
         lines = f.readlines()
     
-    rnd_data: dict[str,dict[tuple[int,int,int,int],list[tuple[int,int,int]]]] = {}
-    hrs_data: dict[str,dict[tuple[int,int,int,int],tuple[int,int,int]]] = {}
-    # Creates a key value dict made up of:
-    # Key:
-    # Value: (choice, fail, time)
+    all_instances = set()
+    data: dict[str,dict[str,list[tuple[int,int,int]]]] = {}
     for line in lines[1:]:
         values = line.split(',')
         params = values[0].split(' ')
         method = params[1]
-        key = (int(params[-5]),int(params[-4]),int(params[-3]),int(params[-2]))
-        value = (int(values[2]),int(values[3]),int(values[4]))
+        instance = ' '.join(params[7:-1])
         
-        if method in rnd_based:
-            if method not in rnd_data:
-                rnd_data[method] = {}
-            
-            if key not in rnd_data[method].keys():
-                rnd_data[method][key] = []
-            
-            rnd_data[method][key].append(value)
-        else:
-            if method not in hrs_data:
-                hrs_data[method] = {}
-            
-            hrs_data[method][key] = value
+        # compile all instances
+        all_instances.add(instance)
+        
+        if method not in data.keys():
+            data[method] = {}
+        if instance not in data[method]:
+            data[method][instance] = []
+        data[method][instance].append((int(values[2]),int(values[3]),int(values[4])))
     
-    
-    
-    header_size = len('maxMarginalStrengthBiasedWheelSelectVal') + 2
+    header_size = max(len(iter) for iter in data.keys()) + 2
     column_size = header_size // 3 + 1
     header_size = 3 * column_size + 2
     method_line = f"|{''.center(header_size)}|"
-    header_line = f"|{'instance'.center(header_size)}|"
-    table_body: dict[tuple[int,int,int,int],str] = {}
-    for method in rnd_based:
+    header_line = f"|{'wtMn wtMx logMn logMx c b'.center(header_size)}|"
+    table_body: dict[str,str] = {}
+    for method in data.keys():
         method_line += f"{method.center(header_size)}|"
         header_line += f"{'choice'.center(column_size)} {'fail'.center(column_size)} {'time'.center(column_size)}|"
-        for instance in instances:
+        for instance in all_instances:
             if instance not in table_body.keys():
-                instance_name = f"{instance[:2]},{instance[2:]}".center(header_size)
+                instance_name = f"{instance}".center(header_size)
                 table_body[instance] = f"|{instance_name}|"
             
-            if len(rnd_data[method][instance]) < 6:
-                table_body[instance] += f"{'TIMED OUT'.center(header_size)}|"
+            # No returns, timed out
+            if instance not in data[method].keys():
+                table_body[instance] += f"{'--'.center(header_size)}|"
                 continue
             
+            if method in heuristic_based:
+                choice_string = f"{data[method][instance][0][0]}".center(column_size)
+                fail_string = f"{data[method][instance][0][1]}".center(column_size)
+                time_string = f"{data[method][instance][0][2]/1000.0:,.3f}".center(column_size)
+                table_body[instance] += f"{choice_string} {fail_string} {time_string}|"
+                continue
+            
+            # Random method that timed out
+            if len(data[method][instance]) < 6:
+                table_body[instance] += f"{'--'.center(header_size)}|"
+                continue
+            
+            # Random method that did not time out
             choices = []
             fails = []
             times = []
-            for c,f,t in rnd_data[method][instance]:
+            for c,f,t in data[method][instance]:
                 choices.append(c)
                 fails.append(f)
                 times.append(t)
@@ -92,25 +79,12 @@ def parse_to_terminal(filename):
             fail_string = f"{get_median(fails)}".center(column_size)
             time_string = f"{get_median(times)/1000.0:,.3f}".center(column_size)
             table_body[instance] += f"{choice_string} {fail_string} {time_string}|"
-            
-    for method in hrs_data:
-        method_line += f"{method.center(header_size)}|"
-        header_line += f"{'choice'.center(column_size)} {'fail'.center(column_size)} {'time'.center(column_size)}|"
-        for instance in instances:
-            if instance not in hrs_data[method].keys():
-                table_body[instance] += f"{'TIMED OUT'.center(header_size)}|"
-                continue
-            # print(hrs_data[method][instance])
-
-            choice_string = f"{hrs_data[method][instance][0]}".center(column_size)
-            fail_string = f"{hrs_data[method][instance][1]}".center(column_size)
-            time_string = f"{hrs_data[method][instance][2]/1000.0:,.3f}".center(column_size)
-            table_body[instance] += f"{choice_string} {fail_string} {time_string}|"
     
     print(method_line)
     print(header_line)
-    print('\n'.join(list(table_body.values())))
-    
-parse_to_terminal("new_shortTable.csv")
+    for k,v in sorted(table_body.items(),key=lambda x: x[0]):
+        print(v)
+
+generic_parse_to_terminal("new_cp_data.csv")
 # parse_to_terminal("new_regular.csv")
 # parse_to_terminal("correct_regular_1.csv")
--- new_memoire_data/slout_to_csv.py
+++ new_memoire_data/slout_to_csv.py
@@ -4,4 +4,34 @@ files = [iter for iter in os.listdir() if iter.startswith('slout')]
 
 file_lines = [open(iter,'r').readlines() for iter in files]
 
+filtered = [[iter.strip() for iter in file if 'JAVA' not in iter] for file in file_lines]
+filtered = [[iter.strip() for iter in file if not iter.startswith('stateMap')] for file in filtered]
+filtered = [[iter.strip() for iter in file if not iter.startswith('[INFO]')] for file in filtered]
+filtered = [[iter.strip() for iter in file if not iter.startswith('c Warning')] for file in filtered]
+filtered = [[iter.strip() for iter in file if not iter.startswith('#sols')] for file in filtered]
+filtered = [[iter.strip() for iter in file if not iter.startswith('completed')] for file in filtered]
+filtered = [[iter.strip() for iter in file if not iter.startswith('slurmstepd')] for file in filtered]
 
+filtered = [[iter for iter in file if iter != '642'] for file in filtered]
+filtered = [[iter for iter in file if iter != 'Restarts'] for file in filtered]
+filtered = [[iter for iter in file if iter != 'No restarts'] for file in filtered]
+filtered = [[iter for iter in file if iter != 'domWdegRandom'] for file in filtered]
+filtered = [[iter for iter in file if iter != 'maxMarginal'] for file in filtered]
+filtered = [[iter for iter in file if iter != 'maxMarginalLDS'] for file in filtered]
+filtered = [[iter for iter in file if iter != 'maxMarginalStrength'] for file in filtered]
+filtered = [[iter for iter in file if iter != 'maxMarginalStrengthLDS'] for file in filtered]
+filtered = [[iter for iter in file if iter != ''] for file in filtered]
+
+filtered = [iter for iter in filtered if len(iter) != 1]
+
+for file in filtered:
+    molecule, weight, logp = file[1].split(',')
+    molecule.strip("'")
+    file.append(weight)
+    file.append(logp)
+    file[2] = file[2].split(': ')[-1]
+    file[3] = file[3].split(': ')[-1]
+    file[4] = file[4].split(': ')[-1]
+
+output = '\n'.join(','.join(iter) for iter in filtered)
+print(output)
